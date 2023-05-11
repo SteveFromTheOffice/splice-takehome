0ad1ce0fd74b573b2c02088055327d86fba1b1d8
SteveFromTheOffice <34364583+SteveFromTheOffice@users.noreply.github.com>
2023-05-11 16:58:28 -0700

    flyout toggles

--- src/components/LayoutSidebar/LayoutSidebar.component.jsx
+++ src/components/LayoutSidebar/LayoutSidebar.component.jsx
@@ -1,7 +1,21 @@
 import style from "./LayoutSidebar.module.scss";
 
 function LayoutSidebar(props) {
-  return <aside className={style.sidebar}>{props.children}</aside>;
+  const handleOnMouseOut = (e) => {
+    // If event is fired from a child element, ignore it.
+    if (e.currentTarget.contains(e.relatedTarget)) return;
+
+    // Pass it up.
+    props.onMouseOut(e);
+  };
+
+  return (
+    <aside
+      className={`${style.sidebar} ${props.isOpen ? style.open : null}`}
+      onMouseOut={handleOnMouseOut}>
+      {props.children}
+    </aside>
+  );
 }
 
 export default LayoutSidebar;
