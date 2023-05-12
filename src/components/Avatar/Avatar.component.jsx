37fe24165dda65a54f4203af724299fa3a3778fe
SteveFromTheOffice <34364583+SteveFromTheOffice@users.noreply.github.com>
2023-05-11 20:43:18 -0700

    Avatar props

--- src/components/Avatar/Avatar.component.jsx
+++ src/components/Avatar/Avatar.component.jsx
@@ -1,13 +1,13 @@
 import style from "./Avatar.module.scss";
 
-function Avatar() {
+function Avatar(props) {
   return (
     <div className={style.avatar}>
       <main>
         <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="avatar" />
-        <span>Title</span>
+        <span title={props.text}>{props.text}</span>
       </main>
-      <aside>5m ago</aside>
+      <aside>{props.subtext}</aside>
     </div>
   );
 }
