d587091068698e572f545d62da226a0ba587b561
SteveFromTheOffice <34364583+SteveFromTheOffice@users.noreply.github.com>
2023-05-11 20:43:59 -0700

    table widget

---
+++ src/components/Table/Table.component.jsx
@@ -0,0 +1,50 @@
+import Avatar from "../Avatar/Avatar.component";
+import Badge from "../Badge/Badge.component";
+import Search from "../Search/Search.component";
+
+import DotsIcon from "../../images/ThreeDots.svg";
+import style from "./Table.module.scss";
+
+export const TableStatus = {
+  IN_PROGRESS: "In Progress",
+  REJECTED: "Rejected",
+  COMPLETED: "Completed",
+  APPROVED: "Approved",
+  PENDING: "Pending",
+};
+
+function Table(props) {
+  return (
+    <section className={style.table}>
+      <header>
+        <h2>{props.title}</h2>
+        <Search />
+        <Badge icon={DotsIcon} />
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Manager</th>
+            <th>Date</th>
+            <th>Amount</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.data.map((row, index) => (
+            <tr key={index}>
+              <td>
+                <Avatar text={row.manager} />
+              </td>
+              <td>{row.date}</td>
+              <td>{row.amount}</td>
+              <td>{row.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+}
+
+export default Table;
--- src/pages/Dashboard/Dashboard.component.jsx
+++ src/pages/Dashboard/Dashboard.component.jsx
@@ -1,9 +1,18 @@
+import Table, {TableStatus} from "../../components/Table/Table.component";
 import style from "./Dashboard.module.scss";
+
+const SPENDING_DATA = [
+  {manager: "Koray Okumus", date: "Jun 24, 2022", amount: 942, status: TableStatus.IN_PROGRESS},
+  {manager: "Natali Craig", date: "Mar 10, 2022", amount: 881, status: TableStatus.REJECTED},
+  {manager: "Olivia Rhye", date: "Nov 10, 2022", amount: 409, status: TableStatus.COMPLETED},
+  {manager: "Orlando Diggs", date: "Dec 20, 2022", amount: 953, status: TableStatus.APPROVED},
+  {manager: "Koray Okumus", date: "Jun 24, 2022", amount: 942, status: TableStatus.PENDING},
+];
 
 function Dashboard() {
   return (
     <main className={style.dashboard}>
-      <h1>Dashboard</h1>
+      <Table title="Project Spendings" data={SPENDING_DATA} />
     </main>
   );
 }
